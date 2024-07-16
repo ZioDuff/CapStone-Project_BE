1c284ae19527497a2107eb846eafbc1d7f433efb
Jacopo De maio <jajodemaio@gmail.com>
2024-07-16 12:54:10 +0200

    setup project

---
+++ src/main/java/JacopoDeMaio/TattooStudio/TattooStudioApplication.java
@@ -0,0 +1,13 @@
+package JacopoDeMaio.TattooStudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TattooStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TattooStudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JacopoDeMaio/TattooStudio/TattooStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package JacopoDeMaio.TattooStudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TattooStudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
