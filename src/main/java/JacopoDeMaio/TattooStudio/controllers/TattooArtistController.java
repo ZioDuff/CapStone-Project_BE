c0e5d9b52e2f2e8f2f210dd6b0e5d45d7cac3ce1
Jacopo De maio <jajodemaio@gmail.com>
2024-07-21 10:54:48 +0200

    POST saveTattooArtist method

--- src/main/java/JacopoDeMaio/TattooStudio/controllers/TattooArtistController.java
+++ src/main/java/JacopoDeMaio/TattooStudio/controllers/TattooArtistController.java
@@ -1,16 +1,9 @@
 package JacopoDeMaio.TattooStudio.controllers;
 
 import JacopoDeMaio.TattooStudio.entities.TattoArtist;
-import JacopoDeMaio.TattooStudio.exceptions.BadRequestException;
-import JacopoDeMaio.TattooStudio.payloads.userDTO.GenericDTO;
-import JacopoDeMaio.TattooStudio.payloads.userDTO.NewGenericResponseDTO;
 import JacopoDeMaio.TattooStudio.services.GenericService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -22,15 +15,6 @@ public class TattooArtistController {
     @Autowired
     private GenericService genericService;
 
-    @PostMapping("enroll/tattooArtist")
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @ResponseStatus(HttpStatus.CREATED)
-    public NewGenericResponseDTO saveTattooArtist(@RequestBody @Validated GenericDTO payload, BindingResult validationResult) {
-        if (validationResult.hasErrors()) {
-            throw new BadRequestException(validationResult.getAllErrors());
-        }
-        return new NewGenericResponseDTO(this.genericService.saveTattooArtist(payload).getId());
-    }
 
     @GetMapping
     public Page<TattoArtist> getUsersList(
